a460e51b1d502b2ea33cbb4dd28cda3ea60a6874
Diegocaceres97 <diegocaceres1997garcia@gmail.com>
2021-04-02 17:33:04 -0500

    refactor: Se empezo a utilizar tailwindcss Se decidio empezar a utilizar twcss #issue1

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        danger: '#f05945'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
